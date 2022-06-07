40e0470be8d95280a2dfd65f17cdcba583c18144
Stefan <stefan.joncic@united.group>
2022-06-07 17:21:03 +0200

    display validation error message

--- src/app/movie/add-movie/add-movie.component.ts
+++ src/app/movie/add-movie/add-movie.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+  FormArray,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { NavbarService } from 'src/app/navbar/services/navbar.service';
@@ -9,21 +15,20 @@ import { MovieService } from '../services/movie-service.service';
 @Component({
   selector: 'app-add-movie',
   templateUrl: './add-movie.component.html',
-  styleUrls: ['./add-movie.component.scss']
+  styleUrls: ['./add-movie.component.scss'],
 })
 export class AddMovieComponent implements OnInit {
-
   genres$!: Observable<Genres[]>;
   selectedGenreValue: any = [];
-
   genreArray: any = [];
+  submitted = false;
 
   movieForm = this.fb.group({
     name: ['', Validators.required],
-    image: ['',Validators.required],
+    image: ['', Validators.required],
     genre: this.fb.array([], [Validators.required]),
-    releaseYear: ['',Validators.required],
-    story: ['',Validators.required]
+    releaseYear: ['', Validators.required],
+    story: ['', Validators.required],
   });
 
   constructor(
@@ -31,43 +36,45 @@ export class AddMovieComponent implements OnInit {
     private movieService: MovieService,
     private navbarServise: NavbarService,
     private fb: FormBuilder
-  ){ }
+  ) {}
 
   ngOnInit() {
-
     this.navbarServise.title.next('Add Movie');
     this.getGenres();
   }
 
-  addMovie(){
-    if(this.movieForm.valid){
-      this.movieService.addMovie(this.movieForm.value)
-        .subscribe(res => {
-          console.log(res);
-          this.movieForm.reset();
-          this.router.navigate(['/']);
-        });
+  addMovie() {
+    this.submitted = true;
+    if (this.movieForm.valid) {
+      this.movieService.addMovie(this.movieForm.value).subscribe((res) => {
+        console.log(res);
+        this.movieForm.reset();
+        this.router.navigate(['/']);
+        this.movieService.getAllMovie();
+      });
     }
   }
 
-  getGenres(){    
+  getGenres() {
     this.genres$ = this.movieService.getGenre();
     console.log(this.genres$);
   }
 
-  changeSelection(data: any){
+  changeSelection(data: any) {
     const genre: FormArray = this.movieForm.get('genre') as FormArray;
 
-    if(data.target.checked){
+    if (data.target.checked) {
       genre.push(new FormControl(data.target.value));
     } else {
-      const index = genre.controls.findIndex(x => x.value === data.target.value);
+      const index = genre.controls.findIndex(
+        (x) => x.value === data.target.value
+      );
       genre.removeAt(index);
     }
     console.log(genre.value);
   }
 
-  resetForm(){
+  resetForm() {
     this.movieForm.reset();
   }
 }

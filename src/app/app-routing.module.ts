711fd78ffbdfb973fbc3d1222417bac0486b1f15
Stefan <stefan.joncic@united.group>
2022-06-07 16:47:46 +0200

    add new routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieWishToWatchComponent } from './movie-wish-to-watch/movie-wish-to-watch.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'movies', pathMatch: 'full'},
   {path: 'movies', loadChildren: () => import('./movie/movie.module').then(m => m.MovieModule)},
+  {path: 'wishToWatch', component: MovieWishToWatchComponent}
 ];
 
 @NgModule({
